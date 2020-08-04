82ad031f9b2803a9a6f64ebd619557c854df5938
Mark <alnyan@airmail.cc>
2020-08-04 16:58:45 +0300

    Move dynamic linking test out of main user tree

--- progs/dyn/test/dyn.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-extern int dyn_add(int x, int y);
-
-int main() {
-    printf("2 + 2 is %d\n", dyn_add(2, 2));
-    return 0;
-}
--- progs/dyn/test/lib.c
+++
@@ -1,7 +0,0 @@
-int dyn_mul(int x, int y) {
-    return x * y;
-}
-
-int dyn_add(int x, int y) {
-    return x + y;
-}
